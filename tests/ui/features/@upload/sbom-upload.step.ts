054e9f0b7bc751aabffc434d98da0764577d716e
Pavel Sedlák <psedlak@redhat.com>
2025-04-29 02:50:26 +0200

    fixup - format fix

--- tests/ui/features/@upload/sbom-upload.step.ts
+++ tests/ui/features/@upload/sbom-upload.step.ts
@@ -8,28 +8,32 @@ export const { Given, When, Then, Step } = createBdd();
 
 // Used for both When and Given
 const visitUploadPage = async ({ page }) => {
-  await page.getByRole("link", { name: "Upload"}).click();
+  await page.getByRole("link", { name: "Upload" }).click();
 
   const header = page.locator(`xpath=(//h1[text()="Upload"])`);
   await header.waitFor({ state: "visible", timeout: 2000 });
 };
 Step("User visits Upload page", visitUploadPage);
 
-Then('SBOM upload tab is selected', async ({ page }) => {
+Then("SBOM upload tab is selected", async ({ page }) => {
   await expect(page.getByRole("tab", { name: "SBOM" })).toHaveAttribute(
     "aria-selected",
     "true"
   );
 });
 
-Then('Drag and drop instructions are visible', async ({ page }) => {
-  await expect(page.getByLabel('SBOM').getByText('Drag and drop files here or')).toBeVisible();
+Then("Drag and drop instructions are visible", async ({ page }) => {
+  await expect(
+    page.getByLabel("SBOM").getByText("Drag and drop files here or")
+  ).toBeVisible();
 });
 
-Then('Upload button is present', async ({ page }) => {
-  await expect(page.getByRole('button', { name: 'Upload' })).toBeVisible();
+Then("Upload button is present", async ({ page }) => {
+  await expect(page.getByRole("button", { name: "Upload" })).toBeVisible();
 });
 
-Then('Accepted file types are described', async ({ page }) => {
-  expect(page.getByLabel('SBOM').getByText('Accepted file types:')).toBeVisible();
+Then("Accepted file types are described", async ({ page }) => {
+  expect(
+    page.getByLabel("SBOM").getByText("Accepted file types:")
+  ).toBeVisible();
 });
